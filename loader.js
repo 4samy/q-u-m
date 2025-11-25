4b1181e4e16ca09bc4df69f823a232b93c61e2c5
4samy <2mfsamy@gmail.com>
2025-11-25 12:58:05 +0400

    Update CDN base URL to new version

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 (function () {
-    const base = "https://cdn.jsdelivr.net/gh/4samy/q-u-m@acfda79/src/";
+    const base = "https://cdn.jsdelivr.net/gh/4samy/q-u-m@5e78edb/src/";
 
     const files = [
         "core/dataFetcher.js",
