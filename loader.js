1e807ab4be5ae1aaed4aa924718bc4e8cb46311e
4samy <2mfsamy@gmail.com>
2025-11-25 11:41:04 +0400

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 (function () {
-    const base = "https://cdn.jsdelivr.net/gh/4samy/quality-ultra-max/src/";
+    const base = "https://cdn.jsdelivr.net/gh/4samy/q-u-m/src/";
 
     const files = [
         "core/dataFetcher.js",
